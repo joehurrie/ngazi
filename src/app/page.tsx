3580d9de4dbf259822b556301bc0a661f5f435df
joharie kisiangani <kisianganijoharie@gmail.com>
2026-01-05 00:31:58 +0000

    Add scroll animations for a smoother experience to put sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,30 @@ import Process from '@/components/landing/process';
 import Services from '@/components/landing/services';
 import Testimonials from '@/components/landing/testimonials';
 import Work from '@/components/landing/work';
+import { AnimateOnScroll } from '@/components/client/animate-on-scroll';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Marquee />
-      <Services />
-      <Work />
-      <Process />
-      <Testimonials />
-      <Faq />
+      <AnimateOnScroll>
+        <Marquee />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Services />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Work />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Process />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Testimonials />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Faq />
+      </AnimateOnScroll>
     </>
   );
 }
