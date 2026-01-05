6fb56e93f4b889dde203508dd13ce85e748bbd38
joharie kisiangani <kisianganijoharie@gmail.com>
2026-01-05 03:49:47 +0000

    On mobile the right column is out cut in half , increase the margin to t

--- src/components/landing/services.tsx
+++ src/components/landing/services.tsx
@@ -72,10 +72,10 @@ export default function Services() {
           </div>
         </div>
         
-        <div className="lg:col-span-1 flex items-center justify-center lg:mt-5 mt-8">
+        <div className="lg:col-span-1 flex items-center justify-center mt-20 lg:mt-5">
           <div className="w-full h-[500px] overflow-hidden flex items-center justify-center">
             <div className="relative h-full wall-container flex justify-center">
-              <div className="wall-grid flex gap-6">
+              <div className="wall-grid flex gap-6 lg:w-auto w-[540px] justify-center">
                 <div className="wall-column-up flex flex-col gap-6">
                   {[...wallCardsCol1, ...wallCardsCol1].map((card, index) => (
                     <WallCard key={`col1-${index}`} icon={card.icon} text={card.text} />
